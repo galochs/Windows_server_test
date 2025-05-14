ce269f4666650f9504a50ab4f517a8dc852c22a0
galochs <138891463+galochs@users.noreply.github.com>
2025-05-14 13:46:13 +0200

    Update TimeZone-check.ps1

--- PSscripts/TimeZone-check.ps1
+++ PSscripts/TimeZone-check.ps1
@@ -1,2 +1,4 @@
-$value = (Get-TimeZone).baseutcoffset -split ':' | Select-Object -First 1
+$hostname = hostname
+$time = (Get-TimeZone).baseutcoffset -split ':' | Select-Object -First 1
+$value = $hostname + " : UTC" + $time
 $value > c:\temp\TimeZone-check.txt
